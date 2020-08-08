5b1ae165b439c41ab27e4df7c6a50a75c470076f
love_stm32 <65066115+lijx60@users.noreply.github.com>
2020-08-09 00:38:03 +0800

    Update stm32.c

--- stm32.c
+++ stm32.c
@@ -6,9 +6,23 @@ int fputc(int ch, FILE *f)
 }
 
 
-
-int fputc(int ch, FILE *f)
+uint16_t HAL_GetTick_us(void)
 {
-      HAL_UART_Transmit(&huart1, (uint8_t *)&ch,1, 0xFFFF);
-      return ch;
+ //返回寄存器值，这里先假设定时器2，要按具体情况定
+   return  __HAL_TIM_GET_COUNTER(&htim2);
 }
+
+// 微秒级延时，更准确。
+ void HAL_Delay_us(uint16_t Delay) //小于65535，要拿到前面去声明一下
+{
+  uint16_t tickstart = HAL_GetTick_us();
+  uint16_t wait = Delay;
+
+     while ((HAL_GetTick_us() - tickstart) < wait)
+		{
+		}
+	HAL_TIM_Base_Stop(&htim2);
+	 __HAL_TIM_SET_COUNTER(&htim2, 0);
+ }
+
+
